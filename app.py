1848c3f7b70390df41122a6f83ba633cb103f449
cbrundige <chrisbrundige@protonmail.com>
2021-06-17 13:50:49 -0400

    Added a login page for HIPAA compliance

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import pandas as pd
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 from functions import runModel
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template("dashboard.html", strokeProb="P(CVA)")
+# Route for handling the login page logic
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', error=error)
+
+
+
 
 
 @app.route('/all-data')
